fd2dcf67091a7b7f15cafb3bfa0dcb7bded127a4
juha <khabh@naver.com>
2024-07-17 18:03:50 +0900

    add

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoomescapeApplication {
 
     public static void main(String[] args) {
-        System.out.println("HELLLLLO");
+        System.out.println("BYEEEEEEEEEEEEE");
         SpringApplication.run(RoomescapeApplication.class, args);
     }
 
